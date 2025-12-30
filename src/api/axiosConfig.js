e1ece74a45f9bab7fe8b577681109a54f4a25182
WinningCloud <farhan84897@gmail.com>
2025-12-30 10:57:28 +0530

    Added backend render url

--- src/api/axiosConfig.js
+++ src/api/axiosConfig.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL:  'http://localhost:4000/api',
+  baseURL:  'https://internship-portal-backend-h46h.onrender.com/api',
 });
 
 // Automatically add JWT to every request
